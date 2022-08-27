bd5716dec42f15dcc4efd1d70bdffeef7148203e
Shane B <37834723+parawanderer@users.noreply.github.com>
2022-08-28 00:50:42 +0200

    extra info that is required

--- codeigniter/app/Views/accessibility.php
+++ codeigniter/app/Views/accessibility.php
@@ -18,6 +18,25 @@
             <p>Our site has been built using code compliant with recent HTML5 and CSS standards and best practices. The site displays 
                 correctly in current browsers as of 27/08/2022, and its use of standards for HTML/CSS will further ensure any future 
                 browsers or versions will display it correctly, too.</p>
+
+            <div class="m-5"></div>
+
+            <h3>Techniques used</h3>
+            <div class="m-4"></div>
+
+            <p>Some of the techniques used to achieve accessibility include the following:</p>
+            <ul>
+                <li class="my-2">We try to use HTML semantics. Standard HTML elements are used to help readability of content using screen readers.</li>
+                <li class="my-2">A modern layout structure is used and supplemented using appropriate elements such 
+                    as <code class="highlighter-rouge">&lt;nav&gt;</code>, <code class="highlighter-rouge">&lt;articles&gt;</code> and <code class="highlighter-rouge">&lt;side&gt;</code>.</li>
+                <li class="my-2">Keyboard-based functionality is added where possible on custom-scripted click-based interactions/features.</li>
+                <li class="my-2">Alternative text on graphical elements is provided where possible such as <code class="highlighter-rouge">alt</code> text 
+                for <code class="highlighter-rouge">&lt;img&gt;</code> and download alternatives for <code class="highlighter-rouge">&lt;video&gt;</code> (see below)</li>
+                <li class="my-2">A main area of performance issues - large media files - are optimized to load smaller appropriate sub-sizes to minimize data sent over the network and thus increase load speed</li>
+                <li class="my-2">Checks were performed against the "Lighthouse" feature of Google Chrome on all pages and "Accessibility" feature of Mozilla Firefox.</li>
+            </ul>
+
+            <div class="m-4"></div>
             
             <div class="m-5"></div>
             <h3>Known Exceptions</h3>
@@ -29,7 +48,8 @@
             <h6>Exceptions Include</h6>
             
             <ul>
-                <li class="my-2">Not being able to provide detailed image or media descriptions on user-generated content. This may result in lack of screen-reader accessible descriptions for some products if users do not provide their own descriptions.</li>
+                <li class="my-2">Not being able to provide detailed image or media descriptions on user-generated content. 
+                    This may result in lack of screen-reader accessible descriptions for some products if users do not provide their own descriptions.</li>
                 <li class="my-2">Some features are provided using dynamic Javascript scripting technology combined with mouse-based 
                     click interactions. Not all such features may be supported by screen readers and tab-based navigation fully.</li>
                 <li class="my-2">While we will provide a direct link to video files uploaded by users, we have been unable to provide 
